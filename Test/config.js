3df9fd15b8d61a4a5860af267edbdc94db9ec623
jinpingshi <leo198451@gmail.com>
2014-05-26 17:09:40 +0800

    wemusic ad test file
    wemusic ad test file

--- Test/config.js
+++ Test/config.js
@@ -5,7 +5,14 @@
            "priority": 1,
            "startTime": 1301091982,
            "endTime": 1501091982,
-           "url": "https://raw.githubusercontent.com/jinpingshi/jinpingshi.github.io/master/Test/adAudioTest.mp3"
+           "url": "https://raw.githubusercontent.com/jinpingshi/jinpingshi.github.io/master/Test/adAudioTest1.mp3"
+           },
+           {
+           "id": 2,
+           "priority": 1,
+           "startTime": 1301091982,
+           "endTime": 1501091982,
+           "url": "https://raw.githubusercontent.com/jinpingshi/jinpingshi.github.io/master/Test/adAudioTest2.mp3"
            }
            ]
 }
