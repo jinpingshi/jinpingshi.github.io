146c3d7b36593e974960cfc5fa8fab3b53f445e7
jinpingshi <leo198451@gmail.com>
2014-05-26 16:18:48 +0800

    add test file
    wemusic ad test file

---
+++ Test/config.js
@@ -0,0 +1,11 @@
+{
+    "ad" :[
+           {
+           "id": 1,
+           "priority": 1,
+           "startTime": 1301091982,
+           "endTime": 1501091982,
+           "url": "https://github.com/jinpingshi/jinpingshi.github.io/blob/master/Test/adAudioTest.mp3"
+           }
+           ]
+}
