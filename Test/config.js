f08b7f994957ce9dc3fa6b5f7ffca9205253c252
jinpingshi <leo198451@gmail.com>
2014-05-27 13:22:25 +0800

    fix config file bug
    fix config file bug

--- Test/config.js
+++ Test/config.js
@@ -11,7 +11,7 @@
            "priority": 1,
            "startTime": 1301091982,
            "endTime": 1501091982,
-           "cover": "https://raw.githubusercontent.com/jinpingshi/jinpingshi.github.io/master/Test/adAudioTest1.jpg"
+           "cover": "https://raw.githubusercontent.com/jinpingshi/jinpingshi.github.io/master/Test/adAudioTest1.jpg",
            "url": "https://raw.githubusercontent.com/jinpingshi/jinpingshi.github.io/master/Test/adAudioTest1.mp3"
            },
            {
@@ -21,7 +21,7 @@
            "priority": 1,
            "startTime": 1301091982,
            "endTime": 1501091982,
-           "cover": "https://raw.githubusercontent.com/jinpingshi/jinpingshi.github.io/master/Test/adAudioTest2.jpg"
+           "cover": "https://raw.githubusercontent.com/jinpingshi/jinpingshi.github.io/master/Test/adAudioTest2.jpg",
            "url": "https://raw.githubusercontent.com/jinpingshi/jinpingshi.github.io/master/Test/adAudioTest2.mp3"
            }
            ]
