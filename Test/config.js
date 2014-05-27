d62feef42568f318c006d4d95627ba9eeb3fc087
jinpingshi <leo198451@gmail.com>
2014-05-27 11:40:10 +0800

    wemusic ad test file
    wemusic ad test file

--- Test/config.js
+++ Test/config.js
@@ -1,17 +1,27 @@
 {
-    "ad" :[
+    "logic": {
+        "duration": 900,
+        "numPlay": 1
+    },
+    "ad": [
            {
            "id": 1,
+           "name": "ad1",
+           "singer": "王铮亮",
            "priority": 1,
            "startTime": 1301091982,
            "endTime": 1501091982,
+           "thumb": "https://raw.githubusercontent.com/jinpingshi/jinpingshi.github.io/master/Test/adAudioTest1.jpg"
            "url": "https://raw.githubusercontent.com/jinpingshi/jinpingshi.github.io/master/Test/adAudioTest1.mp3"
            },
            {
            "id": 2,
+           "name": "ad2",
+           "singer": "邓紫棋",
            "priority": 1,
            "startTime": 1301091982,
            "endTime": 1501091982,
+           "thumb": "https://raw.githubusercontent.com/jinpingshi/jinpingshi.github.io/master/Test/adAudioTest2.jpg"
            "url": "https://raw.githubusercontent.com/jinpingshi/jinpingshi.github.io/master/Test/adAudioTest2.mp3"
            }
            ]
